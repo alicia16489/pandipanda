24fa9a60708eb0f1f3a7f5e7f09576fd1993a30b
Julien Boudry <julien.boudry@supinternet.fr>
2013-05-08 18:45:26 +0200

    modifications auteurs head html

--- templates/head.template.php
+++ templates/head.template.php
@@ -4,7 +4,7 @@
 			<meta charset="UTF-8">
 			<title>OpenEntity</title>
 			<meta name="description" content="Site communautaire de partage de contenus libres autour de l'Univers de MediaEntity.cc">
-			<meta name="author" content="Ciret Benoit,Nicolas portier">
+			<meta name="author" content="Sup'Internet - Groupe d'étudiants OpenEntity : Ciret Benoit, Portier Nicolas, Gios Baptiste, Boudry Julien, Sacerdot Isaure, Lork Mathilde, Thomas Adrien">
 			<meta name="keywords" content="MediaEntity, Fanfictions, Fan Art, Mutation mediatique, bande dessinee">
 			<link rel="stylesheet" type="text/css" media="all" href="styles/style.css">
 </head>
