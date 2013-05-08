1df7121560c6a2d3f5795c339decdc61594f26e4
Julien Boudry <julien.boudry@supinternet.fr>
2013-05-08 18:43:27 +0200

    Mise à jour des keywords

--- templates/head.template.php
+++ templates/head.template.php
@@ -5,7 +5,7 @@
 			<title>OpenEntity</title>
 			<meta name="description" content="Site communautaire de partage de contenus libres autour de l'Univers de MediaEntity.cc">
 			<meta name="author" content="Ciret Benoit,Nicolas portier">
-			<meta name="keywords" content="">
+			<meta name="keywords" content="MediaEntity, Fanfictions, Fan Art, Mutation mediatique, bande dessinee">
 			<link rel="stylesheet" type="text/css" media="all" href="styles/style.css">
 </head>
 	<body>
